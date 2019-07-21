be7550cb72c92c95fa0c920f15cd5932fb7fef0a
odinokun <odinokun@gmail.com>
2019-07-21 11:24:56 +0300

    did preloader

---
+++ src/js/modules/preloader.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+
+  // begin preloader
+  $(document).ready(function () {
+    $('#preloader-wrap').fadeOut();
+    $('#main-wrap').removeClass('no-scroll');
+  });
+  // end preloader
+
+};

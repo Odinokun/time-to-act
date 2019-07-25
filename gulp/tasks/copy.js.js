1bd3b88e178bae014e0abaa7b8768d8c99e82310
odinokun <odinokun@gmail.com>
2019-07-26 01:34:34 +0300

    did contacts page

--- gulp/tasks/copy.js.js
+++ gulp/tasks/copy.js.js
@@ -3,7 +3,10 @@
 module.exports = function() {
 
   $.gulp.task('copy:js', function() {
-    return $.gulp.src('src/js/uncompressed.js')
+    return $.gulp.src([
+      'src/js/uncompressed.js',
+      'src/js/map.js'
+    ])
       .pipe($.gulp.dest($.config.root + '/assets/js'))
   })
 
